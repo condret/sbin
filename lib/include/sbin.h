204e046f5409cb4fdc9558af9776df0a21bee081
condret <condr3t@protonmail.com>
2018-01-23 01:47:43 +0000

    add a few more structs, and a concept for future plugins

--- lib/include/sbin.h
+++ lib/include/sbin.h
@@ -18,6 +18,65 @@ typedef struct s_bin_io_t {
 	SBinIoFdSize fd_size;
 } SBinIo;
 
+typedef struct s_bin_hash_t {
+	const char *type;
+	ut64 addr;
+	int len;
+	ut64 from;
+	ut64 to;
+	ut8 buf[32];
+	const char *cmd;
+} SBinHash;
+
+typedef struct s_bin_symbol_t {
+	char *name;
+	ut64 vaddr;
+	ut64 paddr;
+	ut32 size;
+} SBinSymbol;
+
+typedef bool (*SBinSymbolCb) (void *user, SBinSymbol symbol);
+
+typedef struct s_bin_plugin_t {
+	const char *name;
+	const char *desc;
+	const char *license;
+	bool (*symbols)(void *user, SBinSymbolCb add_symbol, SBinFile *arch);
+	//plugin should go for add_symbol(user, symbol) on each symbol it finds
+} SBinPlugin;
+
+typedef struct s_bin_info_t {
+	char *file;
+	char *type;
+	char *bclass;
+	char *rclass;
+	char *arch;
+	char *cpu;
+	char *machine;
+	char *os;
+	char *subsystem;
+	char *rpath;
+	char *guid;
+	char *debug_file_name;
+	const char *lang;
+	int bits;
+	int has_va;
+	int has_pi; // pic/pie
+	int has_canary;
+	int has_crypto;
+	int has_nx;
+	int big_endian;
+	bool has_lit;
+	char *actual_checksum;
+	char *claimed_checksum;
+	int pe_overlay;
+	bool signature;
+	ut64 dbg_info;
+	SBinHash sum[3];
+	ut64 baddr;
+	char *intrp;
+} SBinInfo;
+
 typedef struct s_bin_file_t {
 	int fd;		//file descriptor
 	ut32 bfd;	//bin-file descriptor
